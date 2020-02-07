272615075a9e7967de692e043d3f58ad89ef865a
lorentzca <kohta@lorentzca.me>
2020-02-08 01:14:38 +0900

    Replace space and enter

--- keyboards/keebio/iris/keymaps/lorentzca/keymap.c
+++ keyboards/keebio/iris/keymaps/lorentzca/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,                           KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴────────┴──────┬─┴─────┐            ┌─────┴──┬─────┴────────┴─────┬──┴─────┬──┴────────┴────────┴────────┘
-                                    KC_LGUI, LT(_LOWER,KC_LANG2), KC_ENT,              KC_SPC,  LT(_RAISE,KC_LANG1), KC_RALT
+                                    KC_LGUI, LT(_LOWER,KC_LANG2), KC_SPC,              KC_ENT,  LT(_RAISE,KC_LANG1), KC_RALT
                                 // └────────┴────────────────────┴───────┘            └────────┴────────────────────┴────────┘
   ),
 
